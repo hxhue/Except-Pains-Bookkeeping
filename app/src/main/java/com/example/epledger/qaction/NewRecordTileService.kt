f1b4e6c9ebeb85339e1d64e18794ee6105c5b43d
Simon Yu <1076830758@qq.com>
2021-04-05 17:44:49 +0800

    (QuickRecord) add tile support

--- app/src/main/java/com/example/epledger/qaction/NewRecordTileService.kt
+++ app/src/main/java/com/example/epledger/qaction/NewRecordTileService.kt
@@ -1,4 +1,15 @@
 package com.example.epledger.qaction
 
-class NewRecordTileService {
+import android.content.Intent
+import android.service.quicksettings.TileService
+
+class NewRecordTileService : TileService() {
+    // 点击启动该卡片
+    override fun onClick() {
+        super.onClick()
+        val recCardIntent = Intent(this, PopupActivity::class.java)
+                .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED)
+                .setAction(Intent.ACTION_MAIN)
+        startActivityAndCollapse(recCardIntent)
+    }
 }
