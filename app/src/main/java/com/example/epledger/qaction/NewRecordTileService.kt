9ee42e7c88429a1d835fba7732c8e87ee8e06c0e
Simon Yu <1076830758@qq.com>
2021-04-22 19:52:05 +0800

    fix: crash when launching two instances of MainActivity

--- app/src/main/java/com/example/epledger/qaction/NewRecordTileService.kt
+++ app/src/main/java/com/example/epledger/qaction/NewRecordTileService.kt
@@ -13,14 +13,14 @@ import java.util.*
  */
 class NewRecordTileService : TileService() {
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Log.d("qaction.NewRecordTileService", "onStartCommand() called at ${System.currentTimeMillis()}")
+        Log.d("NewRecordTileService", "onStartCommand() called at ${System.currentTimeMillis()}")
         return super.onStartCommand(intent, flags, startId)
     }
 
     // 点击启动该卡片
     override fun onClick() {
         super.onClick()
-        Log.d("qaction.NewRecordTileService", "onClicked() called at ${System.currentTimeMillis()}")
+        Log.d("NewRecordTileService", "onClicked() called at ${System.currentTimeMillis()}")
 
         val recCardIntent = Intent(this, PopupActivity::class.java)
                     .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED)
@@ -29,6 +29,8 @@ class NewRecordTileService : TileService() {
         // Slow after pressing HOME from any app(even not from mine!)
         // This is because of Google's concern about app privilege
         // Nothing to do with my code...
-        startActivityAndCollapse(recCardIntent)
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            startActivityAndCollapse(recCardIntent)
+        }
     }
 }
